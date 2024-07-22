493fe0dc9f532293d3c9d939e54cce763f30ece4
Abbbbb <144189002+Abbbbbqvq@users.noreply.github.com>
2024-07-22 11:49:30 +0300

    Update enscan_script.py

--- enscan_script.py
+++ enscan_script.py
@@ -10,6 +10,7 @@ import requests
 import base64
 import json
 import xlwt
+from bs4 import BeautifulSoup
 
 def enscan_domain():
     domain_list = []
@@ -187,7 +188,6 @@ def enscan_run(deep=' '):
             current_line += 1
             print("正在运行enscan，当前正在导出：" + line.replace("\n", "") + "，目前进度：" + "{:.2%}".format(current_line / all_line))
             line = line.replace('\n', '')
-            print(line)
             commond = './enscan -n ' + line + ' -json ' + deep
             print(commond)
             output = subprocess.run(commond, shell=True, capture_output=True, text=True, encoding='utf-8')
@@ -205,17 +205,65 @@ def enscan_run(deep=' '):
 localtime = time.localtime(time.time())
 make_time = time.strftime("%Y%m%d%H%M%S", time.localtime())
 
+def ip_c_search():
+    print("正在查询ip C段")
+    ip_c_file = open(args.output_dir + "/ip_c.txt", "a+", encoding='utf-8')
+    ip_file = open(args.output_dir + "/ip.txt", "r", encoding='utf-8')
+    for ip in ip_file.readlines():
+        t = re.compile(r'\.(\d+)\n')
+        r = t.findall(ip)[0]
+        url = "https://chapangzhan.com/" + ip.replace(r + "\n", "0") + "/24"
+        # 定义header
+        header = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+        }
+        # 发起请求
+        req = requests.get(url, headers=header)
+        bs = BeautifulSoup(req.text, "html.parser")
+        body = str(bs.body)
+        t = re.compile('<a href="https://ipchaxun.com/(.*?)/" rel="nofollow" target="_blank">')
+        r = t.findall(body)
+        for c_ip in r:
+            print("当前正在查询：" + c_ip)
+            ip138_run(c_ip)
+            sleep(1)
+
+def ip138_run(ip):
+    url = "https://site.ip138.com/" + ip + "/"
+    # 定义header
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+    }
+    # 发起请求
+    req = requests.get(url, headers=header)
+    bs = BeautifulSoup(req.text, "html.parser")
+    body = str(bs.body)
+    t = re.compile('<ul id="list">(.*?)</ul>', re.DOTALL)
+    r = t.findall(body)
+    t = re.compile('target="_blank">(.*?)</a></li>', re.DOTALL)
+    domain = t.findall(r[0])
+    for i in domain:
+        print(i)
+
 def hunter_scan():
     global res
     is_null = ["hunter提取为空或是失败的资产："]
     number = 1
     print("正在使用hunter提取domain，请稍等")
-    hunter_write_to_xls()
+    hunter_create_xls()
     api_key = ""
     with open("./" + args.output_dir + "/domain.txt", 'r', encoding='utf-8') as domain_read:
         for line in domain_read.readlines():
             line = line.replace('\n', '')
-            print("正在提取：" + line)
+            print("正在使用hunter提取：" + line)
             query_sentence = 'domain.suffix="' + line + '" + and ip.country=="中国"'
             search = base64.urlsafe_b64encode(query_sentence.encode("utf-8"))
             search = str(search, 'utf8')
@@ -225,7 +273,7 @@ def hunter_scan():
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36',
                 }
-                print("正在提取：" + line + "，当前正在导出第" + str(is_page) + "页")
+                print("正在使用hunter提取：" + line + "，当前正在导出第" + str(is_page) + "页")
                 try:
                     resp = requests.get(url=url, headers=headers)
                     res = json.loads((resp.content).decode('utf-8'))
@@ -241,7 +289,7 @@ def hunter_scan():
                         else:
                             break
                     elif res["data"]["arr"] != None and res["code"] != 400:
-                        number = hunter_scan_and_write(number)
+                        number = hunter_write_xls(number)
                 except:
                     is_null.append(line)
                     print("错误信息：", end="")
@@ -252,13 +300,13 @@ def hunter_scan():
     for i in is_null:
         err_file.write(i + "\n")
     print("提取完毕，正在" + args.output_dir + "目录下生成xls，提取到的ip已经筛选好放入ip.txt")
-    workbook.save(args.output_dir + '/hunter.xls')
+    workbook_for_hunter.save(args.output_dir + '/hunter.xls')
 
-def hunter_write_to_xls():
-    global workbook
-    workbook = xlwt.Workbook(encoding='utf-8')
-    global worksheet
-    worksheet = workbook.add_sheet('鹰图查询结果')
+def hunter_create_xls():
+    global workbook_for_hunter
+    workbook_for_hunter = xlwt.Workbook(encoding='utf-8')
+    global worksheet_for_hunter
+    worksheet_for_hunter = workbook_for_hunter.add_sheet('鹰图查询结果')
     # 创建颜色
     pattern = xlwt.Pattern()  # 创建模式对象
     pattern.pattern = xlwt.Pattern.SOLID_PATTERN
@@ -266,25 +314,25 @@ def hunter_write_to_xls():
     style = xlwt.XFStyle()  # 创建样式对象
     style.pattern = pattern  # 将模式加入到样式对象
     # 设置单元格的宽度
-    worksheet.col(0).width = 400 * 20
-    worksheet.col(1).width = 200 * 20
-    worksheet.col(2).width = 100 * 20
-    worksheet.col(3).width = 400 * 20
-    worksheet.col(4).width = 150 * 20
-    worksheet.col(5).width = 400 * 20
-    worksheet.col(6).width = 400 * 20
-    worksheet.col(7).width = 400 * 20
+    worksheet_for_hunter.col(0).width = 400 * 20
+    worksheet_for_hunter.col(1).width = 200 * 20
+    worksheet_for_hunter.col(2).width = 100 * 20
+    worksheet_for_hunter.col(3).width = 400 * 20
+    worksheet_for_hunter.col(4).width = 150 * 20
+    worksheet_for_hunter.col(5).width = 400 * 20
+    worksheet_for_hunter.col(6).width = 400 * 20
+    worksheet_for_hunter.col(7).width = 400 * 20
     # 写第一行的标题
-    worksheet.write(0, 0, '网址', style)
-    worksheet.write(0, 1, 'IP地址', style)
-    worksheet.write(0, 2, '端口', style)
-    worksheet.write(0, 3, '网站标题', style)
-    worksheet.write(0, 4, '域名', style)
-    worksheet.write(0, 5, '状态码', style)
-    worksheet.write(0, 6, '公司名称', style)
-    worksheet.write(0, 7, '备案号', style)
+    worksheet_for_hunter.write(0, 0, '网址', style)
+    worksheet_for_hunter.write(0, 1, 'IP地址', style)
+    worksheet_for_hunter.write(0, 2, '端口', style)
+    worksheet_for_hunter.write(0, 3, '网站标题', style)
+    worksheet_for_hunter.write(0, 4, '域名', style)
+    worksheet_for_hunter.write(0, 5, '状态码', style)
+    worksheet_for_hunter.write(0, 6, '公司名称', style)
+    worksheet_for_hunter.write(0, 7, '备案号', style)
 
-def hunter_scan_and_write(number):
+def hunter_write_xls(number):
     global l
     l =0
     ip_file = open(args.output_dir + "/ip.txt", "a+", encoding='utf-8')
@@ -302,16 +350,89 @@ def hunter_scan_and_write(number):
         record_number = res["data"]["arr"][l]["number"]  # 备案号
 
         # 写入数据
-        worksheet.write(number, 0, its_url)
-        worksheet.write(number, 1, ip)
-        worksheet.write(number, 2, port)
-        worksheet.write(number, 3, web_title)
-        worksheet.write(number, 4, domain)
-        worksheet.write(number, 5, status_code)
-        worksheet.write(number, 6, company)
-        worksheet.write(number, 7, record_number)
+        worksheet_for_hunter.write(number, 0, its_url)
+        worksheet_for_hunter.write(number, 1, ip)
+        worksheet_for_hunter.write(number, 2, port)
+        worksheet_for_hunter.write(number, 3, web_title)
+        worksheet_for_hunter.write(number, 4, domain)
+        worksheet_for_hunter.write(number, 5, status_code)
+        worksheet_for_hunter.write(number, 6, company)
+        worksheet_for_hunter.write(number, 7, record_number)
         number += 1
     return number
+
+def fofa_scan():
+    global res
+    number = 1
+    fofa_create_xls()
+    fofa_key = ''
+    with open("./" + args.output_dir + "/domain.txt", 'r', encoding='utf-8') as domain_read:
+        for line in domain_read.readlines():
+            line = line.replace('\n', '')
+            print("正在使用fofa提取：" + line)
+            query_sentence = 'domain="' + line + '"'
+            # query_sentence = 'domain = "jsydzb.com"'
+            fofa_search = base64.urlsafe_b64encode(query_sentence.encode("utf-8"))
+            fofa_search = str(fofa_search, 'utf8')
+            fofa_is_page = 1
+            while True:
+                url = 'https://fofa.info/api/v1/search/all?&key=' + str(fofa_key) + '&qbase64=' + str(fofa_search) + "&page=" + str(fofa_is_page) + "&full=true"
+                print(url)
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36',
+                }
+                print("正在使用fofa提取：" + line + "，当前正在导出第" + str(fofa_is_page) + "页")
+                try:
+                    resp = requests.get(url=url, headers=headers)
+                    res = json.loads((resp.content).decode('utf-8'))
+                    print(res)
+                    if len(res['results']) == 0:
+                        break
+                    number = fofa_write_xls(number)
+                except:
+                    print("导出出现错误，请自查：")
+                    print(res)
+                sleep(3)
+                fofa_is_page += 1
+            fofa_for_workbook.save(args.output_dir + "/fofa.xls")
+
+def fofa_create_xls():
+    global fofa_for_workbook
+    fofa_for_workbook = xlwt.Workbook(encoding='utf-8')
+    global worksheet_for_fofa
+    worksheet_for_fofa = fofa_for_workbook.add_sheet('fofa查询结果')
+    # 创建颜色
+    pattern = xlwt.Pattern()  # 创建模式对象
+    pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+    pattern.pattern_fore_colour = 5  # 设置模式颜色为黄色
+    style = xlwt.XFStyle()  # 创建样式对象
+    style.pattern = pattern  # 将模式加入到样式对象
+    # 设置单元格的宽度
+    worksheet_for_fofa.col(0).width = 400 * 20
+    worksheet_for_fofa.col(1).width = 200 * 20
+    worksheet_for_fofa.col(2).width = 100 * 20
+    # 写第一行的标题
+    worksheet_for_fofa.write(0, 0, '网址', style)
+    worksheet_for_fofa.write(0, 1, 'IP地址', style)
+    worksheet_for_fofa.write(0, 2, '端口', style)
+
+def fofa_write_xls(number):
+    ip_file = open(args.output_dir + "/ip.txt", "a+", encoding='utf-8')
+    url_file = open(args.output_dir + "/url.txt", "a+", encoding='utf-8')
+    for l in range(len(res['results'])):
+        its_url = res['results'][l][0]  # 网址
+        url_file.write(its_url + "\n")
+        ip = res['results'][l][1]  # IP地址
+        ip_file.write(ip + "\n")
+        port = res['results'][l][2]  # 端口
+
+        # 写入数据
+        worksheet_for_fofa.write(number, 0, its_url)
+        worksheet_for_fofa.write(number, 1, ip)
+        worksheet_for_fofa.write(number, 2, port)
+        number += 1
+    return number
+
 
 def remove_duplicates():
     ip_file_list = []
@@ -341,7 +462,8 @@ if __name__ == '__main__':
                         help="运行模式：\n例如：python3 main.py -m 1\n1、运行完enscan然后使用hunter提取出资产域名信息。\n"
                              "2、只运行enscan。\n"
                              "3、只提取资产域名信息。\n"
-                             "4、只运行hunter(默认提取result/domain.txt，不是默认目录需要-o指定目录)")
+                             "4、只运行hunter(默认提取result/domain.txt，不是默认目录需要-o指定目录)\n"
+                             "5、只运行fofa(默认提取result/domain.txt，不是默认目录需要-o指定目录)")
     parser.add_argument("-d", "--dir", dest='running_dir',
                         help="指定enscan输出目录\n例如：python3 main.py -d outs(不加-d参数默认提取outs目录下的导出结果)")
     parser.add_argument("-o", "--output", dest='output_dir',
@@ -377,4 +499,7 @@ if __name__ == '__main__':
         enscan_domain()
     elif args.running_mode == "4":
         hunter_scan()
+        remove_duplicates()
+    elif args.running_mode == "5":
+        fofa_scan()
         remove_duplicates()
